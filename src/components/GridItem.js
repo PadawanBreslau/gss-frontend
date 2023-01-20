51ab281c701cf5fe6a9f4116a7b0a647b5439908
Staszek <s.zawadzki@visuality.pl>
2023-01-20 10:46:16 +0100

    More tiles

--- src/components/GridItem.js
+++ src/components/GridItem.js
@@ -4,7 +4,7 @@ const GridItem = ({ counter, src, alt }) => {
   return (
     <>
       {[...Array(counter)].map((e, i) => (
-        <ImageListItem key={i} sx={{border: '1px solid'}}>
+        <ImageListItem key={i} sx={{border: '1px solid', backgroundColor: 'white'}}>
           <img src={src} srcSet={src} alt={alt} loading="lazy" />
         </ImageListItem>
       ))}
--- src/components/Serce.js
+++ src/components/Serce.js
@@ -1,21 +1,46 @@
 import { Box, ImageList, Typography } from "@mui/material";
-import serce from "../images/serce.jpeg";
+import serce from "../images/serce/serce.jpg";
 import gss2 from "../images/logo-green.svg";
-import szybkie from "../images/szybkie.jpg";
-import empty from "../images/white.png";
+import szybkie from "../images/serce/szybkie.jpg";
+import ania from "../images/serce/ania.png";
+import visu from "../images/serce/visu.png";
+import poltrax from "../images/serce/poltrax.png";
+import empty from "../images/serce/white.png";
 import GridItem from "./GridItem";
 
 const Serce = () => {
   return (
     <Box>
-      <a href="https://www.sercedziecka.org.pl/" target="_blank" rel="noreferrer">
-        <Box
-          component="img"
-          className="serce"
-          src={serce}
-          sx={{ width: "50%", mb: 5, margin: 'auto' }}
-        />
+      <Box sx={{margin: 'auto', mb: 10, ml: 20}}>
+      <a
+        href="https://www.sercedziecka.org.pl/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Box component="img" className="serce" src={serce} />
       </a>
+      </Box>
+      <Box sx={{ width: "90%", margin: "auto", mb: 3, mt: 3 }}>
+        <Typography variant="h3">
+          Każdy kwadrat to 1gr za każdy 1km każdego zweryfikowanego turysty na
+          GSS 2.0
+        </Typography>
+      </Box>
+      <ImageList
+        sx={{ width: "90%", margin: "auto" }}
+        rows={10}
+        cols={10}
+        //        rowHeight={80}
+        gap={2}
+      >
+        <GridItem counter={10} src={gss2} alt="GSS 2.0" />
+        <GridItem counter={2} src={szybkie} alt="Szybkie Podróże" />
+        <GridItem counter={1} src={ania} alt="Anka Sekulska" />
+        <GridItem counter={5} src={visu} alt="Visuality" />
+        <GridItem counter={5} src={poltrax} alt="Potrax" />
+        <GridItem counter={77} src={empty} alt="Wolne" />
+      </ImageList>
+
       <Box
         sx={{
           textAlign: "left",
@@ -32,8 +57,12 @@ const Serce = () => {
         </p>
 
         <p>
-          Jako że spodobał mi się sposów w jaki kolega Rafał znany jako &nbsp;
-          <a href="https://www.facebook.com/SzybkiePodroze/" target="_blank" rel="noreferrer">
+          Jako że spodobał mi się sposób w jaki kolega Rafał znany jako &nbsp;
+          <a
+            href="https://www.facebook.com/SzybkiePodroze/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Szybkie Podróże
           </a>
           &nbsp;zbiera pieniądze w swoich akcjach charytatywnych - gdzie
@@ -48,7 +77,11 @@ const Serce = () => {
           rzetelna i przeznaczająca zebrane środki na leczenie dzieci, zakup
           sprzętu i pomoc rodzinom w trudnych chwilach. Więcej informacji na ich
           &nbsp;
-          <a href="https://www.sercedziecka.org.pl/" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.sercedziecka.org.pl/"
+            target="_blank"
+            rel="noreferrer"
+          >
             stronie
           </a>
         </p>
@@ -67,20 +100,6 @@ const Serce = () => {
           <i>Staszek</i>
         </p>
       </Box>
-
-      <Box sx={{width: '60%', margin: 'auto', mb: 3}}>
-        <Typography variant="h3">Każdy kwadrat to 1gr za każdy 1km każdego zweryfikowanego turysty na GSS 2.0</Typography>
-      </Box> 
-      <ImageList
-        sx={{width: '60%', margin: 'auto'}} 
-        rows={10}
-        cols={10}
-        gap={2}
-      >
-        <GridItem counter={10} src={gss2} alt="GSS 2.0"/>
-        <GridItem counter={2} src={szybkie} alt="Szybkie Podróże"/>
-        <GridItem counter={88} src={empty} alt="Wolne"/>
-      </ImageList>
     </Box>
   );
 };
